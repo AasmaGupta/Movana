a6eb2e23c70100dad5284d80f993cbcef73a6b64
Aasma Gupta <aasmagupta1971@gmail.com>
2026-07-02 07:41:45 +0530

    UI changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -15,7 +15,7 @@ function Dashboard() {
 
             <div className="dashboard-grid">
 
-                <div className="card" onClick={() => navigate("/bookcargo")}> <h3>Book Cargo</h3> </div>
+                <div className="cardp" onClick={() => navigate("/bookcargo")}> <h3>Book Cargo</h3> </div>
 
                 <div className="cardp" onClick={() => navigate('/bookmetro')}><h3>Book Metro</h3></div>
 
